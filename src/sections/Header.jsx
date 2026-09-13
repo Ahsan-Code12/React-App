d9765f45572426d425ad2f370dd44ea8be5b2df3
Ahsan-Code12 <none31ae@gmail.com>
2026-09-14 01:18:36 +0500

    update

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -1,6 +1,6 @@
 const Header = () => {
     return (
-        <header>
+        <header className= "fixed top-0 left-0 z-50 w-full py-50">
             <div>
                 <a>
                 <img src="/images/xora.svg" width={115} height={55} alt="Logo"/>
